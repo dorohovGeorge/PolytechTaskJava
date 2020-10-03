2bd180bc3b850212c56ba21a5b3faa2d5ceb36c1
dorohovGeorge <20gora17@gmail.com>
2020-10-03 19:24:29 +0300

    Task 1. Maybe done

--- src/task1/taskMixingElementsInArray.java
+++ src/task1/taskMixingElementsInArray.java
@@ -1,11 +1,22 @@
 package task1;
 
+import java.util.*;
+
 public class taskMixingElementsInArray {
     public static void main(String[] args) {
-
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        test(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    public static void mixElementsInArray(int[] array)
-    {
 
+    public static void test(int[] arr) {
+        List<Integer> list = new ArrayList<>(arr.length);
+        for (int i : arr) {
+            list.add(i);
+        }
+        Collections.shuffle(list);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
     }
 }
